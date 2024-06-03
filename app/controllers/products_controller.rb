50f2fc0f14809f65ac240e1cdff72741511efa11
wakiy1031 <wakiy1031@gmail.com>
2024-06-03 21:14:38 +0900

    with_attached_imgにする

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    @latest_product = Product.order(created_at: :desc).limit(4).where.not(id: @product.id).distinct
+    @latest_product = Product.with_attached_img.order(created_at: :desc).limit(4).where.not(id: @product.id).distinct
   end
 
   def create
