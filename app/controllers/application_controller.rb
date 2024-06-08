3c6815120e6ecfbcdff14006a1f8120e2cd2233d
wakiy1031 <wakiy1031@gmail.com>
2024-06-08 23:50:27 +0900

    セッションの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :set_cart
   before_action :current_cart
+  before_action :set_cart
   before_action :set_cart_products
 
   private
 
-  def set_cart
-    @cart = Cart.find(session[:cart_id])
-  end
-
   # セッションを使用して現在のカートを取得または作成する
   def current_cart
     @current_cart ||= Cart.find_or_create_by(id: session[:cart_id])
-    session[:cart_id] ||= @current_cart.id
+    session[:cart_id] = @current_cart.id
     @current_cart
+  end
+
+  def set_cart
+    @cart = Cart.find(session[:cart_id])
   end
 
   def set_cart_products
